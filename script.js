6a7cc69a405e606a0d6c869b41e530c4b8bfe555
hemamohnani <mohnanihema0@gmail.com>
2021-07-18 00:38:21 +0530

    xyz

---
+++ script.js
@@ -0,0 +1,25 @@
+let counters = document.querySelectorAll('.counter')
+
+counters.forEach((counter)=>{
+    console.log(counter)
+    counter.innerHTML=0;
+
+    const updateCounter = () =>{
+        const targetcount = +counter.getAttribute('data-target');
+        console.log(typeof targetcount); //this data is a string,  you can check the type using typeof
+
+        const startingCount = Number(counter.innerHTML)
+        const incr = targetcount/100;
+        if(startingCount < targetcount)
+        {
+        counter.innerHTML=`${Math.round(startingCount+incr)}`;
+        setTimeout(updateCounter,10)
+        }
+        else
+        {
+            counter.innerHTML = targetcount
+        }
+    }
+
+    updateCounter();
+})
